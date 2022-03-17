e19a97f8bd31d810d8c03d2d3d36a056632865e0
KamLar <kamil.larysz.gra2@gmail.com>
2022-03-17 14:20:52 +0100

    basic bench for the golang template

---
+++ direct/tpl_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+	"text/template"
+)
+
+var benchTempl *template.Template
+var benchData interface{}
+var benchWriter MockWriter
+
+func init() {
+	var err error
+	benchTempl, err = template.ParseFiles("./tpl.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	type Data struct {
+		Var1   int
+		Var2   int
+		Values []string
+	}
+
+	values := make([]string, 100)
+	for i, _ := range values {
+		values[i] = "Var" + strconv.Itoa(i+1)
+	}
+
+	benchData = &Data{
+		Var1:   10,
+		Var2:   5,
+		Values: values,
+	}
+}
+
+func BenchmarkDirectTempl(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		benchTempl.Execute(benchWriter, benchData)
+	}
+}
+
+type MockWriter struct {
+}
+
+func (m MockWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
